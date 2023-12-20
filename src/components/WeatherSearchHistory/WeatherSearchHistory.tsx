f1acf21d89c04f80399b6f6c330e0ed286478e8c
Viet Huynh <xa.lach@yahoo.com>
2023-12-21 02:36:32 +0700

    handle empty history

--- src/components/WeatherSearchHistory/WeatherSearchHistory.tsx
+++ src/components/WeatherSearchHistory/WeatherSearchHistory.tsx
@@ -13,15 +13,22 @@ const WeatherSearchHistory = (props: WeatherSearchHistoryProps) => {
       <h3 className='mb-4 py-2 font-bold text-2xl border-b border-b-solid'>
         Search History
       </h3>
-      <div>
-        {entries.map((entry, index) => (
-          <HistoryEntryRow
-            key={`${entry.city}-${entry.country}-${entry.time.toString()}`}
-            index={index + 1}
-            entry={entry}
-          />
-        ))}
-      </div>
+
+      {entries.length === 0 && (
+        <div className='text-center text-2xl'>No Record</div>
+      )}
+
+      {entries.length > 0 && (
+        <div>
+          {entries.map((entry, index) => (
+            <HistoryEntryRow
+              key={`${entry.city}-${entry.country}-${entry.time.toString()}`}
+              index={index + 1}
+              entry={entry}
+            />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
